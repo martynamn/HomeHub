8ea97322ce716627f4f106ab3180e452e83e53cf
Mateusz_Lagiewka <marquess1224@gmail.com>
2024-11-18 17:42:01 +0100

    hh-5 added GET by userId and property create method

--- Backend/myapp/services/property.py
+++ Backend/myapp/services/property.py
@@ -2,10 +2,20 @@ from operator import itemgetter
 
 from django.db.models import Q
 from django.http import JsonResponse
+from pymongo import MongoClient
+from django.conf import settings
+from bson.objectid import ObjectId
+from bson.json_util import dumps
 
-from myapp.models import Property
-from myapp.serializer import PropertySerializer
+from myapp.models import Property, Image, Address
+from myapp.serializer import PropertySerializer, ImageSerializer, AddressSerializer
+import base64
+import json
+import base64
+from datetime import datetime
 
+client = MongoClient(settings.DATABASES['default']['CLIENT']['host'])
+db = client[settings.DATABASES['default']['NAME']]
 
 def get_last_properties(number: int, user_id: str, ):
     properties = Property.objects.filter(Q(adType__in=['sale', 'rent']), userId=user_id)
@@ -20,8 +30,74 @@ def get_last_properties(number: int, user_id: str, ):
 
 def get_properties_by_user(user_id: str):
     properties = Property.objects.filter(userId=user_id)
-    if not properties:
-        return JsonResponse({'error': 'No properties found for this user'}, status=404)
 
     properties_list = PropertySerializer(properties, many=True).data
     return JsonResponse(properties_list, safe=False)
+
+def get_property_by_id(property_id):
+    try:
+        property_data = db.PROPERTY.find_one({'_id': ObjectId(property_id)})
+
+        if not property_data:
+            return {'error': 'Property not found'}
+
+        address_data = db.ADDRESS.find_one({'_id': property_data['address']})
+        property_data['address'] = address_data
+
+        image_data = db.IMAGE.find_one({'_id': property_data['image']})
+        property_data['image'] = image_data
+
+        property_data['_id'] = str(property_data['_id'])
+        property_data['address']['_id'] = str(property_data['address']['_id'])
+        property_data['image']['_id'] = str(property_data['image']['_id'])
+
+        return property_data
+
+    except Exception as e:
+        return {'error': str(e)}
+
+
+def create_property(metadata, files):
+    try:
+        data = json.loads(metadata)
+
+        address_data = {
+            'country': data['address']['country'],
+            'city': data['address']['city'],
+            'postcode': data['address']['postcode'],
+            'floor': data['address']['floor']
+        }
+        address_id = db.ADDRESS.insert_one(address_data).inserted_id
+
+        images = []
+        for file in files:
+            file_content = file.read()
+            encoded_image = base64.b64encode(file_content).decode('utf-8')
+            image_data = {
+                'imageData': encoded_image,
+                'filename': file.name
+            }
+            image_id = db.IMAGE.insert_one(image_data).inserted_id
+            images.append(image_id)
+
+        property_data = {
+            'description': data.get('description'),
+            'title': data.get('title'),
+            'type': data.get('type'),
+            'adType': data.get('adType'),
+            'userId': data.get('userId'),
+            'price': data.get('price'),
+            'rooms': data.get('rooms'),
+            'creationDate': datetime.now(),
+            'address': address_id,
+            'image': images[0] if images else None
+        }
+        property_id = db.PROPERTY.insert_one(property_data).inserted_id
+        return {
+            'success': True,
+            'message': 'Property created successfully!',
+            'property': str(property_id)
+        }
+
+    except Exception as e:
+        return {'error': str(e)}
--- Backend/myproject/urls.py
+++ Backend/myproject/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from myapp.views import get_dashboard, get_revenue_for_year
 from myapp.views import get_last_properties
 from myapp.views import get_properties_by_user
+from myapp.views import get_property_by_id
+from myapp.views import create_property_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +13,9 @@ urlpatterns = [
     path('api/dashboard/revenue/<int:year>', get_revenue_for_year),
     path('api/property/latest/<int:number>', get_last_properties),
     path('api/property/user/<str:user_id>', get_properties_by_user),
+    path('api/property/<str:property_id>', get_property_by_id),
+    path('api/property', create_property_view)
 ]
+
+
+
