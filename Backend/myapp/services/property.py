9f5a7d5b0ce7377366728a889c5a45133cbceb2f
Mateusz Młynek <75077558+MateuszMlynek0@users.noreply.github.com>
2024-12-11 22:11:40 +0100

    dev to master (#4)
    * Rebase commit
    
    * Changes
    
    * hh-5 added GET by userId
    
    * hh-5 added GET by userId and property create method
    
    * hh-7: Added property and profile api methods
    
    ---------
    
    Co-authored-by: Mateusz_Lagiewka <marquess1224@gmail.com>

--- Backend/myapp/services/property.py
+++ Backend/myapp/services/property.py
@@ -3,13 +3,21 @@ from operator import itemgetter
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 from django.http import JsonResponse
-from myapp.models import Property
-from myapp.serializer import PropertySerializer
+from pymongo import MongoClient
+from django.conf import settings
+from bson.objectid import ObjectId
 
+from myapp.models import Property, Image, Address
+from myapp.serializer import PropertySerializer, ImageSerializer, AddressSerializer
+import base64
+import json
+import base64
+from datetime import datetime
+
+client = MongoClient(settings.DATABASES['default']['CLIENT']['host'])
+db = client[settings.DATABASES['default']['NAME']]
 
 def get_last_properties(number: int, user_id: str, ):
-    if not number:
-        number = 3
     properties = Property.objects.filter(Q(adType__in=['sale', 'rent']), userId=user_id)
     if not properties:
         return JsonResponse({'error': 'No properties found for this user'}, status=404)
@@ -20,6 +28,13 @@ def get_last_properties(number: int, user_id: str, ):
     last_properties = properties_list[:number]
     return JsonResponse(last_properties, safe=False)
 
+def get_properties_by_user(user_id: str):
+    properties = Property.objects.filter(userId=user_id)
+    if not properties:
+        return JsonResponse({'error': 'No properties found for this user'}, status=404)
+
+    properties_list = PropertySerializer(properties, many=True).data
+    return JsonResponse(properties_list, safe=False)
 
 def get_properties(request):
     filters = {
@@ -78,3 +93,170 @@ def get_paginated_properties(properties, request):
         properties_page = paginator.page(paginator.num_pages)
 
     return properties_page
+
+
+def get_properties(request):
+    filters = {
+        "userId": request.GET.get('userId', None),
+        "type": request.GET.get("type", None),
+        "adType": request.GET.get("ad_type", None),
+        "address__country": request.GET.get("country", None),
+        "address__city": request.GET.get("city", None),
+        "address__floor": int(request.GET.get("floor", )) if request.GET.get("floor", None) is not None else None,
+        "rooms": int(request.GET.get("rooms", None)) if request.GET.get("rooms", None) is not None else None,
+        "price__gte": int(request.GET.get("min_price", None)) if request.GET.get("min_price",
+                                                                                 None) is not None else None,
+        "price__lte": int(request.GET.get("max_price", None)) if request.GET.get("max_price",
+                                                                                 None) is not None else None,
+        "area__gte": int(request.GET.get("min_area", None)) if request.GET.get("min_area", None) is not None else None,
+        "area__lte": int(request.GET.get("max_area", None)) if request.GET.get("max_area", None) is not None else None,
+    }
+
+    filters = {filter: value for filter, value in filters.items() if value is not None}
+
+    q_filter = Q()
+    for field, value in filters.items():
+        if value is not None:
+            q_filter &= Q(**{field: value})
+    q_filter &= ~Q(adType="sold")
+
+    properties = Property.objects.filter(q_filter)
+    serialized_properties = PropertySerializer(get_paginated_properties(properties, request), many=True).data
+
+    return JsonResponse(serialized_properties, safe=False)
+
+
+def get_paginated_properties(properties, request):
+    limit = request.GET.get('limit')
+    index = request.GET.get('index')
+
+    limit = int(limit) if limit is not None else 10
+    index = int(index) if index is not None else 1
+
+    paginator = Paginator(properties, limit)
+
+    try:
+        properties_page = paginator.page(index)
+    except PageNotAnInteger:
+        properties_page = paginator.page(1)
+    except EmptyPage:
+        properties_page = paginator.page(paginator.num_pages)
+
+    return properties_page
+
+
+def get_property_by_id(property_id):
+    try:
+        property_data = db.PROPERTY.find_one({'_id': ObjectId(property_id)})
+
+        if not property_data:
+            return {'error': 'Property not found'}
+
+        address_data = db.ADDRESS.find_one({'_id': property_data['address']})
+        property_data['address'] = address_data
+
+        image_data = db.IMAGE.find_one({'_id': property_data['image']})
+        property_data['image'] = image_data
+
+        property_data['_id'] = str(property_data['_id'])
+        property_data['address']['_id'] = str(property_data['address']['_id'])
+        property_data['image']['_id'] = str(property_data['image']['_id'])
+
+        return property_data
+
+    except Exception as e:
+        return {'error': str(e)}
+
+
+def create_property(metadata, files):
+    try:
+        data = json.loads(metadata)
+
+        address_data = {
+            'country': data['address']['country'],
+            'city': data['address']['city'],
+            'postcode': data['address']['postcode'],
+            'floor': data['address']['floor']
+        }
+        address_id = db.ADDRESS.insert_one(address_data).inserted_id
+
+        images = []
+        for file in files:
+            file_content = file.read()
+            encoded_image = base64.b64encode(file_content).decode('utf-8')
+            image_data = {
+                'imageData': encoded_image,
+                'filename': file.name
+            }
+            image_id = db.IMAGE.insert_one(image_data).inserted_id
+            images.append(image_id)
+
+        property_data = {
+            'description': data.get('description'),
+            'title': data.get('title'),
+            'type': data.get('type'),
+            'adType': data.get('adType'),
+            'userId': data.get('userId'),
+            'price': data.get('price'),
+            'rooms': data.get('rooms'),
+            'creationDate': datetime.now(),
+            'address': address_id,
+            'image': images[0] if images else None
+        }
+        property_id = db.PROPERTY.insert_one(property_data).inserted_id
+        return {
+            'success': True,
+            'message': 'Property created successfully!',
+            'property': str(property_id)
+        }
+
+    except Exception as e:
+        return {'error': str(e)}
+
+
+def delete_property_by_id(property_id):
+    try:
+        result = db.PROPERTY.delete_one({'_id': ObjectId(property_id)})
+
+        if result.deleted_count == 0:
+            return {'error': 'Property not found'}
+
+        return {'success': True, 'message': 'Property deleted successfully'}
+
+    except Exception as e:
+        return {'error': str(e)}
+
+
+def sold_property_by_id(property_id):
+    try:
+        result = db.PROPERTY.find_one_and_update(
+            {'_id': ObjectId(property_id)},
+            {'$set': {'adType': 'sold'}}
+        )
+
+        if result is None:
+            return {'error': 'property not found'}
+
+        return {'detail': 'property updated'}
+
+    except Exception as e:
+        return {'error': str(e)}
+
+
+def update_property_by_id(property_id, metadata, files):
+    try:
+        property_data = create_property(metadata, files)
+        result = db.PROPERTY.find_one_and_update(
+            {'_id': ObjectId(property_id)},
+            {'$set': property_data},
+            return_document=True
+        )
+
+        if result is None:
+            return {'error': 'property not found'}
+
+        return {'detail': 'property updated'}
+
+    except Exception as e:
+        return {'error': str(e)}
+
--- Backend/myproject/urls.py
+++ Backend/myproject/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 
-from myapp.views import get_dashboard, get_revenue_for_year, get_last_properties, get_filter_parameter, get_properties, \
-    get_property
+from myapp.views import (get_properties_by_user,
+                         get_property,
+                         get_filter_parameter,
+                         get_properties,
+                         create_property_view,
+                         delete_property_by_id,
+                         sold_property_by_id,
+                         update_property,
+                         get_dashboard,
+                         get_revenue_for_year,
+                         get_last_properties)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,5 +21,12 @@ urlpatterns = [
     path('api/filter', get_filter_parameter),
     path('api/property', get_properties),
     path('api/property/<str:property_id>', get_property),
-
+    path('api/property/user/<str:user_id>', get_properties_by_user),
+    path('api/properties', create_property_view),
+    path('api/property/delete/<str:property_id>', delete_property_by_id),
+    path('api/property/sold/<str:property_id>', sold_property_by_id),
+    path('api/property/update/<str:property_id>', update_property),
 ]
+
+
+
